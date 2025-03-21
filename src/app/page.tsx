1858e0f3110cac1d9b5f3e2045a8562a82b8205e
Bhupathi <bhupathiarikatla2005@gmail.com>
2025-03-21 22:51:20 +0530

    The final changes with no errors1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Navbar from "../app/components/navbar";
-import Footer from "@/app/components/footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Footer />
     </>
   );
 }
